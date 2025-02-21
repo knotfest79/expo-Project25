18d38919f856a1d075f9bed1bd9b4fcc405b22e0
asterik <vikingsfortune@gmail.com>
2025-02-21 11:06:03 +1030

    second commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
 import {View, Text, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Login () {
   return (
-    <View>
-        <Text>
-            Login </Text>
-        <Link href = "/">
-        <Text>Go to Login</Text>
-        </Link>
-    </View>
+    <SafeAreaView>
+        <View>
+            <Text>
+                Login </Text>
+            <Link href = "/">
+            <Text>Go to Login</Text>
+            </Link>
+        </View>
+    </SafeAreaView>
   )
 }
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,15 +1,18 @@
 import {View, Text, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SignUp () {
   return (
-    <View>
-        <Text>
-            Sign up
-        </Text>
-         <Link href = "/signup">
-                <Text>Go to Sign up</Text>
-                </Link>
-    </View>
+    <SafeAreaView>
+        <View>
+            <Text>
+                Sign up
+            </Text>
+             <Link href = "/signup">
+                    <Text>Go to Sign up</Text>
+                    </Link>
+        </View>
+    </SafeAreaView>
   )
 }
