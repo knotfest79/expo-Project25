64c405c02d6fb99ea6fabda26c6f8021d8cacf85
asterik <vikingsfortune@gmail.com>
2025-02-28 10:54:23 +1030

    class time commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { Client, Account, ID } from "react-native-appwrite";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  //initialise AppWrite client
+  const client = new Client()
+    .setProject("67c0eb1a00105d9a93fb")
+    .setPlatform("au.edu.nsw.ait.testapp");
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,14 +38,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack> */}
-      <Stack  screenOptions={{
-        headerShown: false
-      }}/>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,34 +1,70 @@
-import {View, Text,TextInput, StyleSheet } from 'react-native'
-import { Link } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import { useState, useEffect } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FormLabel } from "@/components/FromLabel";
 
-export default function SignUp () {
+export default function SignUp() {
+  const [email, SetEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <SafeAreaView style = {styles.container}>
-        <View >
-            <Text style ={styles.title}>
-                Sign up
-            </Text>
-             <Link href = "/">
-                    <Text>Go to Sign up</Text>
-                    </Link>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.title}>Sign up</Text>
+        <FormLabel color="black" bg="transparent" text="Email" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(val) => SetEmail(val)}
+        />
+        <FormLabel color="black" bg="transparent" text="Password" />
+
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(val) => setPassword(val)}
+        />
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </Pressable>
+        <Link href="/">
+          <Text>Go to login</Text>
+        </Link>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FFDE59"
-    },
-    form: {
-        marginTop : 50,
-    },
-    title: {
-        fontSize: 32,
-        textAlign: "center"
-    }
-
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#FFDE59",
+  },
+  form: {
+    marginTop: 50,
+    marginHorizontal: 40,
+  },
+  title: {
+    fontSize: 32,
+    textAlign: "center",
+  },
+  input: {
+    fontSize: 18,
+    backgroundColor: "white",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 5,
+  },
+  button: {
+    marginVertical: 15,
+    padding: 10,
+    backgroundColor: "hsl(64, 60%, 30%)",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "hsl(64, 60%, 90%)",
+  },
+});
